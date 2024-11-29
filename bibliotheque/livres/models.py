b18714e7a4cca3b392c63bdf7689396bbff73ef4
kacem <kacemmeherzi52@gmail.com>
2024-11-29 09:52:17 +0100

    models created

--- bibliotheque/livres/models.py
+++ bibliotheque/livres/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+#required in forms , null in model 
 # Create your models here.
+class Livre(models.Model):
+    titre = models.CharField(max_length=255)
+    auteur = models.CharField(max_length=255)
+    date_publication = models.DateField()
+    disponible = models.BooleanField(default=True)
+
+class Emprunt(models.Model):
+    livre = models.ForeignKey(Livre, on_delete=models.CASCADE)
+    nom_emprunteur = models.CharField(max_length=255)
+    date_emprunt = models.DateField(auto_now_add=True)
