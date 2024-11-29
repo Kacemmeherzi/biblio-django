3760f06bb1294fabb5c498db4b9dc3dd135789d4
kacem <kacemmeherzi52@gmail.com>
2024-11-29 09:52:40 +0100

    getallbooks method added

--- bibliotheque/livres/views.py
+++ bibliotheque/livres/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from livres.models import Livre
+
 # Create your views here.
+def liste_livres(request):
+    livres = Livre.objects.filter(disponible=True)
+    return render(request, 'livres.html', {'livres': livres})
+def livre_listes_alt (request) : 
+    livres = Livre.objects.exclude(disponible=False)
+    return render(request, 'livres.html', {'livres': livres})
