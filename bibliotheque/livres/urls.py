9cc29016ee5fc81b26b5eeebf9fa7d6f12685051
kacem <kacemmeherzi52@gmail.com>
2024-11-29 11:03:00 +0100

    livre urls added

--- bibliotheque/livres/urls.py
+++ bibliotheque/livres/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('all/', views.liste_livres, name='liste_livres'),
+    path('create/', views.create_livre, name='create'),
+    path('livre/delete/<int:pk>/', views.delete_livre, name='delete_livre'),
+    path('livre/update/<int:pk>/', views.update_livre, name='update_livre'),
+
+
 ]
